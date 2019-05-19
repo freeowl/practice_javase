81ac26d41493c6dff0a207ab767b9b543b4ad345
duwubin <duwubin@fmcc.boss>
2019-05-19 20:48:34 +0800

    git practice

---
+++ src/com/freeowl/javase/HelloWord.java
@@ -0,0 +1,15 @@
+package com.freeowl.javase;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hellow Word");
+        int a =1;
+        int b=a+1;
+        sayBye();
+    }
+
+    public static void sayBye()
+    {
+        System.out.println("bye bye ~");
+    }
+}
